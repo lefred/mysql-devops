28ef58a041b16bb15c67490be9d1e5d07d68a677
Frederic -lefred- Descamps <lefred@percona.com>
2012-10-31 23:15:45 +0100

    fix typo
    Signed-off-by: Frederic -lefred- Descamps <lefred@percona.com>

--- tests/digest.rb
+++ tests/digest.rb
@@ -7,7 +7,7 @@ env.vms.values.each do |vm|
    if vm.created?
 	if vm.state == :running
 	  puts "is running"
-          vm.channel.sudo("pt-query-digest /vagrant/tests/slow.log  --execute='h=localhost,u=jenkins,p=jenkins' | grep 'Exec time' | awk 'NR >1 { print $5 }' >/vagrant/results.new")
+          vm.channel.sudo("pt-query-digest /vagrant/tests/slow.log  --execute='h=localhost,u=jenkins,p=jenkins' | grep 'Exec time' | awk 'NR >1 { print $5 }' >/vagrant/digest.new")
         end
    end
 end
